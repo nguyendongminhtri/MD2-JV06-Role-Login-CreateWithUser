cec2305088085da79d6d7ef1290c335539ee1ca3
nguyendongminhtri29092014@gmail.com <nguyendongminhtri29092014@gmail.com>
2022-08-23 11:16:18 +0700

    Role Create With User

---
+++ src/rikkei/academy/service/user/IUserService.java
@@ -0,0 +1,12 @@
+package rikkei.academy.service.user;
+
+import rikkei.academy.model.User;
+import rikkei.academy.service.IGenericService;
+
+public interface IUserService extends IGenericService<User> {
+    boolean existedByUsername(String username);
+    boolean existedByEmail(String email);
+    boolean checkLogin(String username, String password);
+    User findByUsername(String username);
+    User getCurrentUser();
+}
